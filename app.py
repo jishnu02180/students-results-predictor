f594d4a27226ef8ce80ced8fe18c634c5701a9c9
Jishnnu Sardar <jishnu.sardar@bjitgroup.com>
2022-05-15 21:29:27 +0600

    File rename

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask,request,jsonify
-import pickle
 import numpy as np
+import pickle
 
 model = pickle.load(open('model.pkl','rb'))
 
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello world ML Jishnu"
+    return "Hello world"
 
 @app.route('/predict',methods=['POST'])
 def predict():
@@ -17,7 +17,9 @@ def predict():
     profile_score = request.form.get('profile_score')
 
     input_query = np.array([[cgpa,iq,profile_score]])
+
     result = model.predict(input_query)[0]
+
     return jsonify({'placement':str(result)})
 
 if __name__ == '__main__':
